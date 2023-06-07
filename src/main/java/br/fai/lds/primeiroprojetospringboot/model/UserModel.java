76289628233d0de86c54f02e3364e4c97646e107
Nathan Faria <nathanfaria607@gmail.com>
2023-06-06 22:09:08 -0300

    feat: add user model and game model

---
+++ src/main/java/br/fai/lds/primeiroprojetospringboot/model/UserModel.java
@@ -0,0 +1,49 @@
+package br.fai.lds.primeiroprojetospringboot.model;
+
+public class UserModel {
+    private int id;
+    private String fullName;
+    private String userName;
+    private String password;
+    private String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
