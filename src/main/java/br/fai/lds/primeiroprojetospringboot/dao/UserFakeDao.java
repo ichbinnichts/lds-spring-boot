4000d796222c671419fc22c13cd9799ca88719a9
Nathan Faria <nathanfaria607@gmail.com>
2023-06-06 22:43:38 -0300

    feat: add user fake dao

--- src/main/java/br/fai/lds/primeiroprojetospringboot/dao/UserFakeDao.java
+++ src/main/java/br/fai/lds/primeiroprojetospringboot/dao/UserFakeDao.java
@@ -4,10 +4,17 @@ import br.fai.lds.primeiroprojetospringboot.model.UserModel;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Map;
 
 @Repository
-public class UserDao {
+public class UserFakeDao {
 
+
+    public UserFakeDao(Map<Integer, UserModel> database) {
+        this.database = database;
+    }
+
+    private final Map<Integer, UserModel> database;
 
     public List<UserModel> find() {
         return null;
--- src/main/java/br/fai/lds/primeiroprojetospringboot/service/UserService.java
+++ src/main/java/br/fai/lds/primeiroprojetospringboot/service/UserService.java
@@ -1,6 +1,6 @@
 package br.fai.lds.primeiroprojetospringboot.service;
 
-import br.fai.lds.primeiroprojetospringboot.dao.UserDao;
+import br.fai.lds.primeiroprojetospringboot.dao.UserFakeDao;
 import br.fai.lds.primeiroprojetospringboot.model.UserModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,9 +12,9 @@ public class UserService {
 
 
     @Autowired
-    private UserDao userDao;
+    private UserFakeDao userFakeDao;
 
     public List<UserModel> find() {
-        return userDao.find();
+        return userFakeDao.find();
     }
 }
