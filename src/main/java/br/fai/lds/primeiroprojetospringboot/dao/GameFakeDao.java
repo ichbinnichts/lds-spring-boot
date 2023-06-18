119eb8cd4d1286e03fd2ab7394ad099ed71a9d5a
Nathan Faria <nathanfaria607@gmail.com>
2023-06-18 17:39:47 -0300

    feat: add GameFakeDao

---
+++ src/main/java/br/fai/lds/primeiroprojetospringboot/dao/GameFakeDao.java
@@ -0,0 +1,36 @@
+package br.fai.lds.primeiroprojetospringboot.dao;
+
+import br.fai.lds.primeiroprojetospringboot.model.Game;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class GameFakeDao {
+    private final Map<Integer, Game> db;
+    public GameFakeDao(Map<Integer, Game> database){
+        this.db = database;
+    }
+    public void generateData(){
+
+    }
+    //Get all
+    public List<Game> getAllGames(){
+        List<Game> list = new ArrayList<>();
+        list.addAll(this.db.values());
+        return list;
+    }
+    //Get by id
+    public Game getGameById(int id){
+        Game game = this.db.get(id);
+        return game;
+    }
+    //Update
+    public void updateGame(int id, Game game){
+        this.db.put(game.getId(), game);
+    }
+    //Delete
+    public void deleteGame(int id){
+        this.db.remove(id);
+    }
+}
